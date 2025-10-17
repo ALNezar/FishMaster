f340a2b6038a53d70eb03a1eb71c4eb36c0cdfa6
ALNezar <AbdallaNezaree@gmail.com>
2025-10-17 20:32:58 +0800

    feat(backend): step up springboot backend server

---
+++ backend/src/main/java/com/fishmaster/backend/HomeController.java
@@ -0,0 +1,13 @@
+package com.fishmaster.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}

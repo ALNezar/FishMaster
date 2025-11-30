48647d60a3e8361f86bd13f88ef357213e1a3bbf
ALNezar <AbdallaNezaree@gmail.com>
2025-11-30 19:28:53 +0800

    fix(backend,Jwt): fixed WeakKeyException by adding 32 characters long

--- backend/src/main/java/com/fishmaster/backend/controllers/MessageController.java
+++ backend/src/main/java/com/fishmaster/backend/controllers/MessageController.java
@@ -4,6 +4,8 @@ import com.fishmaster.backend.model.Message;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// This a just a test controller to check if the backend is working.
+
 @RestController
 public class MessageController {
     @RequestMapping("/hello")
@@ -11,5 +13,4 @@ public class MessageController {
         return new Message("Hello Fish!");
 
     }
-
 }

259e217a003349ec72f2ccb186a75736b7b735dc
Abdalla Elshiekh <AbdallaNezaree@gmail.com>
2025-12-10 11:36:16 +0800

    style: update index.html title and clean up unused CSS files

--- frontend/FishMaster/src/NavBar.jsx
+++ frontend/FishMaster/src/NavBar.jsx
@@ -1,12 +1,13 @@
+import './NavBar.css';
+
 export default function NavBar() {
   return (
     <nav className="navbar">
       <ul className="navbar-links">
         <li><a href="#home">Home</a></li>
-        <li><a href="#what-is-fishmaster">What is FishMaster?</a></li>
-        <li><a href="#about-fish">About Fish</a></li>
+        <li><a href="#about">about</a></li>
+        <li><a href="#Features">Features</a></li>
         <li><a href="#contact">Contact Us</a></li>
-        <li><a href="#about-us">About Us</a></li>
       </ul>
     </nav>
 );

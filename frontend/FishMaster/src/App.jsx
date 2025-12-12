a66421ef959f869d75cfeee027a4622c54bb1afc
Abdalla Elshiekh <AbdallaNezaree@gmail.com>
2025-12-12 17:58:19 +0800

    style: Add new SVG asset, button component, and navigation bar with styles
    - Added a new SVG asset for black and red fish.
    - Created a reusable Button component with size and color variants.
    - Implemented a NavBar component for smooth scrolling navigation.
    - Added styles for the Button and NavBar components using SCSS.
    - Introduced animations and mixins for better styling management.
    - Defined global variables for colors, typography, spacing, and shadows.

--- frontend/FishMaster/src/App.jsx
+++ frontend/FishMaster/src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
-import './App.scss'
-import NavBar from './pages/landing page/NavBar.jsx';
+import { useState } from 'react';
+import './App.scss';
+import Button from './components/common/button/button.jsx';
+import Nav from './components/common/nav/NavBar.jsx';
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Nav/>
       
+      <Button>Home</Button>
+      <Button onClick={() => console.log("clicked")}>Get Started!</Button>
+      <Button size="small">Small</Button>
+      <Button variant="accent">Accent</Button>
+      <Button variant="small">gaming</Button>
 
- 
-      
+
+
+      <Button onClick={() => setCount(count + 1)}>
+        count is {count}
+      </Button>
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/FishMaster/src/main.jsx
+++ frontend/FishMaster/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App.jsx";
-import NavBar from "./pages/landing page/NavBar.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <NavBar />
+   
     <App />
+    
   </StrictMode>
 );

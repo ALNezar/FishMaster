9075e9cdf29a79ab33bf74ead37cd914f0cf093b
Abdalla Elshiekh <AbdallaNezaree@gmail.com>
2025-12-13 15:20:53 +0800

    feat(frontend): add react-wavify for wave background effect and update styles

--- frontend/FishMaster/src/App.jsx
+++ frontend/FishMaster/src/App.jsx
@@ -1,42 +1,50 @@
 import React from 'react';
+import './index.scss';
 import './App.scss';
-
+import { CustomScroll } from "react-custom-scroll";
 import ScrollProgress from './components/common/nav/ScrollProgress.jsx';
 import Button from './components/common/button/button.jsx';
 import Card from './components/common/card/card.jsx';
 
 import FishLogo from './assets/images/Fishlogo.svg';
 import BlackRedFish from './assets/images/blackandredfish.svg';
+import Wave from 'react-wavify';
 
 function App() {
   return (
     <>
-      {/* Scroll indicator must be global */}
       <ScrollProgress />
 
-      <div className="landing-container">
+      {/* Wave Background */}
+      <div className="wave-background">
+        <Wave
+          fill="#1277b0"
+          paused={false}
+          options={{
+            height: -11,
+            amplitude: 30,
+            speed: 0.15,
+            points: 5
+          }}
+        />
         
+      </div>
+
+      <div className="landing-container">
         {/* HERO */}
         <section id="home">
           <Card className="hero-card">
             <div className="logo-area">
-              <img
-                src={FishLogo}
-                alt="Fish Master Logo"
-                className="logo"
-              />
+              <img src={FishLogo} alt="Fish Master Logo" className="logo" />
             </div>
-
             <p className="slogan-secondary">
               Monitor your aquarium easily, anytime, anywhere.
             </p>
-
-            <Button className="cta-button">Get Started!</Button>
-
+            <Button className="cta-button">Get Started!
+              (sign up)
+            </Button>
             <p className="login-link-container">
-              <a href="/login" className="login-link">
-                Already have an account? Log in
-              </a>
+              <a href="/login" className="login-link">Already have an account? Log in</a>
             </p>
           </Card>
         </section>
@@ -44,29 +52,15 @@ function App() {
         {/* ABOUT */}
         <section id="about">
           <Card className="about-card">
-            <Button className="about-button">About</Button>
-
-            <h2 className="about-heading">
-              what is <span className="fish-text">FishMaster </span> anyways?
-            </h2>
-
-            <p className="about-intro">TO BEGIN....</p>
-
-            <p className="fish-intro">
-              THIS IS OUR HUMBLE FRIEND THE{' '}
-              <span className="fish-highlight">FISH</span>
-            </p>
-
+           
+              
             <div className="fish-illustration">
-              <img
-                src={BlackRedFish}
-                alt="Fish illustration"
-                className="small-fish"
-              />
+              <img src={BlackRedFish} alt="Fish illustration" className="small-fish" />
             </div>
           </Card>
         </section>
 
+          
       </div>
     </>
   );

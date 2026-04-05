65b89ded7818f8a799b602070f549907af3c754e
ALNezar <AbdallaNezaree@gmail.com>
2026-04-05 14:47:47 +0800

    feat: update temperature sensor configuration and initialization

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,12 +1,27 @@
-#include <Arduino.h> 
+#include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// CHANGE THIS:
+#define TEMP_SENSOR_PIN 32   
+
+OneWire oneWire(TEMP_SENSOR_PIN);
+DallasTemperature tempSensor(&oneWire);
 
 void setup() {
   Serial.begin(115200);
-  delay(1000); 
-  Serial.println("ESP32 Test Started!");
+  delay(2000);
+  tempSensor.begin();
+  Serial.println("Temperature Sensor Ready!");
 }
 
 void loop() {
-  Serial.println("System Running...");
-  delay(2000); 
+  tempSensor.requestTemperatures();
+  float temp = tempSensor.getTempCByIndex(0);
+  
+  Serial.print("Temp: ");
+  Serial.print(temp);
+  Serial.println(" C");
+  
+  delay(2000);
 }

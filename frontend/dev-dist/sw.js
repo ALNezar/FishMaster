2cd553347d4f5644e514e2ea1db2d9889321a899
ALNezar <AbdallaNezaree@gmail.com>
2026-06-02 14:33:05 +0800

    refactor(fortned):deleted the edu section to be replaced by the speices lab

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rneoc3k33gg"
+    "revision": "0.hna3qh4jtmg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
---
+++ frontend/src/utils/push.ts
@@ -0,0 +1,18 @@
+/**
+ * Utility function to convert a Base64URL string to a Uint8Array,
+ * required by the Push API when passing the applicationServerKey.
+ */
+export function urlB64ToUint8Array(base64String: string): Uint8Array {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
